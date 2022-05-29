8a93888aa9670b9f500f99b517b30ede7db7dd39
rintujoyv <rintujoy@yahoo.com>
2022-05-29 13:55:53 -0300

    Loan Calculation Program

---
+++ src/dynamicInput/LoanCalculation.java
@@ -0,0 +1,24 @@
+package dynamicInput;
+
+import java.util.Scanner;
+
+public class LoanCalculation {
+	public static void main(String[] args) {
+		double loanAmount = 0;
+
+		System.out.println("Enter the loan amount");
+
+		Scanner sc = new Scanner(System.in);
+
+		loanAmount = sc.nextDouble();
+
+		for (int i = 1; i <= 3; i++) {
+			System.out.println("Month " + i);
+			System.out.println("Payment: 10% of " + loanAmount + " =" + loanAmount / 10);
+			System.out.println("Remaining amount: " + (loanAmount - (loanAmount / 10)));
+			loanAmount = loanAmount - (loanAmount / 10);
+		}
+
+	}
+
+}
